f652408a131afcfe728443d412037a9050b0b1b5
SAMUEL LEE <samuellee@nyu.edu>
2022-08-13 22:24:56 +0900

    Update CheckAnswer.R
    added return wrong answers function

--- CheckAnswer.R
+++ CheckAnswer.R
@@ -3,17 +3,21 @@ answer <- list_input1
 studentans <- list_input2
 o <- 0 
 x <- 0
-for (ans in 1:length(list_input)) {
+wrong <- c()
+for (ans in 1:length(list_input1)) {
     if (answer[ans] == studentans[ans]) {
       o <- o + 1
     }
-    if (length(answer) != length(studentans)) {
+    else if (length(answer) != length(studentans)) {
       print("length does not match")
       break
     }
-    else {
+    else{
       x <- x + 1
+      wrong[length(wrong)+1] <- ans
     }
 }
 print(paste("no of correct answers:", o, "/", length(list_input1)))
+print("wrong answers:")
+print(wrong)
 }
